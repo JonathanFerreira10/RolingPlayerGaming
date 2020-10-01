f5004e37b14f59a8a45b6d371d4e318c4496c2d3
JonathanFerreira10 <johnnybravo.jc@hotmail.com>
2020-10-01 00:51:08 -0300

    Deploy3

--- src/Pages/home/home.component.ts
+++ src/Pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from '../../app/atributos';
+import { FormControl, FormGroup, ReactiveFormsModule  } from '@angular/forms'; 
+
 
 @Component({
   selector: 'app-home',
@@ -8,12 +11,32 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  usuario: Array<Usuario> = [];
+
+  usuarioForm = new FormGroup({
+    email: new FormControl(''), 
+    senha: new FormControl('')
+  })
+
+
+incluirUsuario(){
+  let pessoa: Usuario = new Usuario(); 
+  //Console Log para mostrar no terminar do Google o Email e senha que está sendo digitado. 
+  console.log(this.usuarioForm.value['email']); 
+  console.log(this.usuarioForm.value['senha']); 
+  pessoa.email = this.usuarioForm.value['email'];
+  pessoa.senha = this.usuarioForm.value['senha'];  
+  this.usuario.push(pessoa); 
+  
+}
+
+
   constructor(
     private router: Router,
   ) {}
 
   ngOnInit(): void {
-    //this.router.navigate(['']);
+    //this.router.navigate(['']); 
 
   }
 
